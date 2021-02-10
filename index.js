0f7302c443a3e0dc7c0797cb9bee0f272a065652
Adrian Lobo <alf8aml04@gmail.com>
2021-02-10 15:21:53 +0000

    replies "present arms" if someone says salute

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-    console.log('Ready!');
+  console.log('Ready!');
 })
 
-client.login('TOKEN');
+client.login(process.env.TOKEN);
 
 client.on('message', message => {
-    console.log(message.content);
+  console.log(message.content);
+  
+  if (message.content === 'salute') {
+    message.channel.send('Present Arms!');
+  }
 })
 
-if (message.content === 'salute') {
-    message.channel.send('Present Arms!');
-}
+
