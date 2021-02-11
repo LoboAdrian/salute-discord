d3cbcb74b525a299df500854ff89ab7015ccfd2d
Adrian Lobo <alf8aml04@gmail.com>
2021-02-10 21:35:30 -0500

    adding the salute command

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const { prefix } = require('./config.json');
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -11,10 +13,17 @@ client.once('ready', () => {
 client.login(process.env.TOKEN);
 
 client.on('message', message => {
-  console.log(message.content);
-  
-  if (message.content === 'salute') {
-    message.channel.send('Present Arms!');
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(' ');
+  const command = args.shift().toLowerCase();
+
+  else if (command === 'args-info') {
+    if (!args.length) {
+      return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+    }
+
+    message.channel.send(`Command name: ${command}\nArguments: ${args}`);
   }
 })
 
